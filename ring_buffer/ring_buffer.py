220ce689dfc67981b87a6e36627938999dbc7029
hectorsvill <hectorstevenvillasno@gmail.com>
2019-10-18 08:25:48 -0700

    ring buffer start

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,23 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    if self.current < self.capacity:
+      self.storage.append(item)
+      self.current += 1
 
   def get(self):
-    pass
+    if self.current == 0:
+      return []
+    return self.storage
+
+if __name__ == "__main__":
+
+
+    buffer = RingBuffer(3)
+
+    
+    print(buffer.get())
+
+
+
+
